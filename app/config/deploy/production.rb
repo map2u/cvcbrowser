57c9c3e0b000fe8dacfe96956587507d6fd0a8c0
Joseph Zhao <zhaoshuilin2004@yahoo.ca>
2015-03-01 21:35:54 -0500

    2015-03-01

--- app/config/deploy/production.rb
+++ app/config/deploy/production.rb
@@ -1,5 +1,5 @@
 server 'cvc.juturna.ca', :app, :web, :primary => true
-set :deploy_to, "/media/web_documents/cvcbrowser1.0/production/"
-set   :current_path, "/media/web_documents/cvcbrowser1.0/production/current"
+set :deploy_to, "/opt/cvc.juturna.ca/production/"
+set   :current_path, "/opt/cvc.juturna.ca/production/current"
 
 after 'deploy:finalize_update', 'symfony:project:clear_controllers'
